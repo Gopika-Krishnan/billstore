6ac6e356b77498ab161f2cd681d8ed5f295da316
Gopika Krishnan <gk1656@nyu.edu>
2021-11-28 20:38:45 +0400

    added video + final touches

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 window.onload = function(){
 let modalBtn = document.getElementById("modal-btn")
 let modalBtn2 = document.getElementById("modal-btn2")
+let modalBtn3 = document.getElementById("modal-btn3")
 
 let modalG = document.querySelector(".modal-gallery")
 let modalF = document.querySelector(".modal-finance")
+let modalFilm = document.querySelector(".modal-film")
 
 let closeBtnF = document.querySelector(".close-btn-finance")
 let closeBtnG = document.querySelector(".close-btn-gallery")
+let closeBtnFilm = document.querySelector(".close-btn-film")
 
 modalBtn.onclick = function(){
   modalF.style.display = "block"
@@ -14,6 +17,9 @@ modalBtn.onclick = function(){
 modalBtn2.onclick = function(){
     modalG.style.display = "block"
   }
+modalBtn3.onclick = function(){
+  modalFilm.style.display = "block"
+}
 
 
 closeBtnG.onclick = function(){
@@ -24,6 +30,11 @@ closeBtnF.onclick = function(){
 modalF.style.display = "none"
 }
 
+closeBtnFilm.onclick = function(){
+  modalFilm.style.display = "none"
+  document.getElementById('movie').pause();
+  }
+  
 window.onclick = function(e){
   if(e.target == modal){
     modal.style.display = "none"
